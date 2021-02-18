5aff1548ea666e45f557f84e152152fa29f5d9c5
evgeny_mordyasov <michisig11i7@gmail.com>
2021-02-18 12:33:38 +0700

    add field 'game' and initiliaze

--- src/Hippodrome.java
+++ src/Hippodrome.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Hippodrome
 {
     private List<Horse> horses;
-
+    public static Hippodrome game;
 
     public Hippodrome(List horses)
     {
@@ -14,7 +14,13 @@ public class Hippodrome
 
     public static void main(String[] args)
     {
+        List<Horse> horses = new ArrayList<>();
 
+        horses.add(new Horse("Плотва 1", 3.0, 0.0));
+        horses.add(new Horse("Плотва 2", 3.0, 0.0));
+        horses.add(new Horse("Плотва 3", 3.0, 0.0));
+
+        Hippodrome.game = new Hippodrome(horses);
     }
 
 
